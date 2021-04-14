d8da9e771799e82d2569077c7f7d91632ee12dd5
John Jones <7935829+acer0001@users.noreply.github.com>
2021-04-14 00:00:10 -0600

    Spelling change
    Changes "what" to "as".

--- pyth0n_crash_course/TIY-2-6.py
+++ pyth0n_crash_course/TIY-2-6.py
@@ -3,7 +3,7 @@
 first_name = "forrest"
 last_name = "gump"
 famous_person = f"{first_name} {last_name}"
-message = f'{famous_person.title()} once said, "Stupid is what stupid does sir!"'
+message = f'{famous_person.title()} once said, "Stupid is as stupid does sir!"'
 
 print(message)
 
