90139fee1c9b34df46d4170f839a6c0fcda7cc46
ralpmetS <7935829+acer0001@users.noreply.github.com>
2023-07-31 15:32:31 -0600

    Create base64_unconvert.py

---
+++ utilities/base64_unconvert.py
@@ -0,0 +1,13 @@
+import base64
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('input_str')
+args = parser.parse_args()
+arg1 = args.input_str
+
+decoded  = arg1.encode('ascii')
+bse64_bytes = base64.b64decode(decoded)
+bse64_message = bse64_bytes.decode('ascii')
+
+print(arg1, ':', bse64_message)
